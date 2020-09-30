5d7c8f8318a1a3d2b17b5b371a27fe16299a2c3f
Kali-Design <hm_leo@hotmail.com>
2020-09-30 23:29:02 +0200

    maj

--- src/expressTestSever.js
+++ src/expressTestSever.js
@@ -1,6 +1,6 @@
 /* Ecrivez un script testServer.js qui se connectera aux différentes url de votre serveur et 
 qui affichera dans la console les messages récupérés. testServer.js utilisera l
-e package axios pour éffectuer les requêtes HTTP sur votre serveur express*/import express from 'express'
+e package axios pour éffectuer les requêtes HTTP sur votre serveur express*/
 
 import fs from 'fs/promises'
 import axios from 'axios'
